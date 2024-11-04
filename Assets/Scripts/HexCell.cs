efe1d88a6f079adfe498cfd57ccc72cb11b3534b
EricTheGloomy <bartosz.wittner@gmail.com>
2024-11-04 15:32:22 +0100

    Other coordinates conversions

--- Assets/Scripts/HexCell.cs
+++ Assets/Scripts/HexCell.cs
@@ -3,25 +3,58 @@ using UnityEngine;
 
 public class HexCell
 {
-    public HexGrid Grid { get; private set; }          // Reference to the HexGrid this cell belongs to
-    public float HexSize { get; private set; }         // Size of the hex cell
-    public TerrainData TerrainType { get; private set; } // Terrain data reference for this cell
+    public HexGrid Grid { get; private set; }
+    public float HexSize { get; private set; }
+    public HexTileData TerrainType { get; private set; }
+    public Vector2 OffsetCoordinates { get; private set; }
+    public Vector3 CubeCoordinates { get; private set; }
+    public Vector2 AxialCoordinates { get; private set; }
+    public List<HexCell> Neighbors { get; private set; } = new List<HexCell>();
 
-    public Vector2 OffsetCoordinates { get; private set; } // Offset coordinates on the grid
-    public Vector3 CubeCoordinates { get; private set; }   // Cube coordinates for 3D hex calculations
-    public Vector2 AxialCoordinates { get; private set; }  // Axial coordinates for 2D hex layout
+    public HexCell(HexGrid grid, Vector2 offsetCoords, HexTileData terrainType)
+    {
+        Grid = grid;
+        OffsetCoordinates = offsetCoords;
+        TerrainType = terrainType;
+        AxialCoordinates = OffsetToAxial(offsetCoords);
+        CubeCoordinates = AxialToCube(AxialCoordinates);
+    }
 
-    public List<HexCell> Neighbors { get; private set; }   // List of neighboring cells
+    private Vector2 OffsetToAxial(Vector2 offsetCoords)
+    {
+        float q = offsetCoords.x - (offsetCoords.y - ((int)offsetCoords.y & 1)) / 2f;
+        float r = offsetCoords.y;
+        return new Vector2(q, r);
+    }
 
-    // Is Constructor needed???
+    private Vector3 AxialToCube(Vector2 axialCoords)
+    {
+        float x = axialCoords.x;
+        float z = axialCoords.y;
+        float y = -x - z;
+        return new Vector3(x, y, z);
+    }
 
-    // Method to add a neighbor to this cell
+    private Vector2 CubeToAxial(Vector3 cubeCoords)
+    {
+        float q = cubeCoords.x;
+        float r = cubeCoords.z;
+        return new Vector2(q, r);
+    }
+
+    private Vector2 CubeToOffset(Vector3 cubeCoords)
+    {
+        float col = cubeCoords.x + (cubeCoords.z - ((int)cubeCoords.z & 1)) / 2f;
+        float row = cubeCoords.z;
+        return new Vector2(col, row);
+    }
+
     public void AddNeighbor(HexCell neighbor)
     {
         if (!Neighbors.Contains(neighbor))
         {
             Neighbors.Add(neighbor);
-            neighbor.Neighbors.Add(this); // Also add this cell as a neighbor of the other cell
+            neighbor.Neighbors.Add(this);
         }
     }
 }
